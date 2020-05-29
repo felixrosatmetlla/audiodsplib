6ac36dd7385f1e1d69ad8cb379cb5fc22f31add7
felixrosatmetlla <felixrosat@gmail.com>
2020-05-29 17:44:42 +0200

    Implement arrayutils.go unit tests

--- audiodsputils/arrayutils.go
+++ audiodsputils/arrayutils.go
@@ -1,15 +1,9 @@
 package audiodsputils
 
-func transformRange(valueToTransform float64, actualMin float64, actualMax float64, newMin float64, newMax float64) float64 {
-	var newValue float64 = ((valueToTransform-actualMin)/(actualMax-actualMin))*(newMax-newMin) + newMin
-
-	return newValue
-}
-
 // GetArrayMinMax returns the minimum and maximum value of an array
-func GetArrayMinMax(array []float64) (float64, float64) {
-	var max float64 = array[0]
-	var min float64 = array[0]
+func GetArrayMinMax(array []float64) (min float64, max float64) {
+	max = array[0]
+	min = array[0]
 
 	for _, value := range array {
 		if max < value {
@@ -27,7 +21,6 @@ func GetArrayMinMax(array []float64) (float64, float64) {
 // Returns a bool to indicate if the signals are equal or not
 func CompareMonoSignals(a, b []float64) bool {
 
-	// If one is nil, the other must also be nil.
 	if (a == nil) != (b == nil) {
 		return false
 	}
