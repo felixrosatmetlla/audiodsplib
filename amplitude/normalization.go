aaf8d51cf581057e4bdf30c0c1ed2b92daf4bc61
felixrosatmetlla <felixrosat@gmail.com>
2020-05-29 18:31:32 +0200

    Implemented RMSNormalization method Unit test of RMSNormalization method

--- amplitude/normalization.go
+++ amplitude/normalization.go
@@ -1,6 +1,8 @@
 package amplitude
 
 import (
+	"math"
+
 	"github.com/felixrosatmetlla/audiodsplib/audiodsputils"
 )
 
@@ -17,4 +19,21 @@ func PeakNormalization(signal []float64) []float64 {
 	return output
 }
 
-//TODO: Implement RMS normalization
+// RMSNormalization normalizes a signal amplitude to a specified RMS (Root Mean Squared) amplitude value
+func RMSNormalization(signal []float64, rmsAmplitude float64) []float64 {
+	var signalLength = len(signal)
+	var output = make([]float64, signalLength)
+
+	var samplesSquaredSum float64 = 0
+	for index := range signal {
+		samplesSquaredSum = samplesSquaredSum + math.Pow(signal[index], 2)
+	}
+
+	var scale = math.Sqrt((float64(signalLength) * rmsAmplitude) / samplesSquaredSum)
+
+	for index := range output {
+		output[index] = signal[index] * scale
+	}
+
+	return output
+}
