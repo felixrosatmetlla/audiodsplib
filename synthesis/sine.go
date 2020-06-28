babf8ca6de2ad0bf85ae45b3cbd4ae08136aab79
felixrosatmetlla <felixrosat@gmail.com>
2020-06-28 16:18:38 +0200

    Modify sinus synthesis to be compatible wiht N-Channels

--- synthesis/sine.go
+++ synthesis/sine.go
@@ -2,24 +2,39 @@ package synthesis
 
 import (
 	"math"
+
+	"github.com/felixrosatmetlla/audiodsplib/types"
 )
 
-// SynthMonoSinus synthesizes a mono sinus signal
+// SynthSinus synthesizes a mono sinus signal
 //
 // Input variables:
 //  frequency: signal frequency in Hz
+//  amplitude: amplitude of the output signal
 //  phase: initial phase offset of the signal
 //  duration: signal duration in seconds
 //  sampleRate: signal sample rate in samples/s
-func SynthMonoSinus(frequency float64, amplitude float64, phase float64, duration float64, sampleRate float64) []float64 {
-	var samples int = int(duration * sampleRate)
-	var sinBuffer = make([]float64, samples)
+//  channels: number of channels of the output signal
+func SynthSinus(frequency float64, amplitude float64, phase float64, duration float64, sampleRate float64, channels int) types.Signal {
+	var numSamplesChannel int = int(duration * sampleRate)
+	var numSamplesSignal int = numSamplesChannel * channels
+	var sinBuffer = make([]float64, numSamplesSignal)
 	var Ts = 1 / sampleRate
 
-	for index := range sinBuffer {
-		var time float64 = float64(index) * Ts
-		sinBuffer[index] = amplitude * math.Sin(2*math.Pi*frequency*time+phase)
+	for channel := 0; channel < channels; channel++ {
+		for index := 0; index < numSamplesChannel; index++ {
+			var sample int = index + channel*numSamplesChannel
+
+			var time float64 = float64(sample) * Ts
+			sinBuffer[sample] = amplitude * math.Sin(2*math.Pi*frequency*time+phase)
+		}
 	}
 
-	return sinBuffer
+	sinus := types.Signal{
+		Data:       sinBuffer,
+		Channels:   channels,
+		Samplerate: sampleRate,
+	}
+
+	return sinus
 }
