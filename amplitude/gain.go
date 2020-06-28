a90bb1b74d33bdc5b9a3abde92e3e4f9f3d2b7d6
felixrosatmetlla <felixrosat@gmail.com>
2020-06-28 17:48:38 +0200

    Improve gain methods documentation

--- amplitude/gain.go
+++ amplitude/gain.go
@@ -9,6 +9,10 @@ import (
 //TODO: MultiChannel options
 
 // ChangeGain modifies the signals gain using a linear value of Amplitude
+//
+// Input variables:
+//  signal: Input signal to modify amplitude
+//  gain: Value of gain that will change the signal
 func ChangeGain(inputSignal types.Signal, gain float64) types.Signal {
 	var bufferSize = inputSignal.NumSamples * inputSignal.Channels
 	var outputBuffer = make([]float64, bufferSize)
@@ -28,6 +32,10 @@ func ChangeGain(inputSignal types.Signal, gain float64) types.Signal {
 }
 
 // ChangeGaindB modifies the signals gain using a value in decibels as input
+//
+// Input variables:
+//  signal: Input signal to modify amplitude
+//  dBChange: Value of decibels that will change the input signal
 func ChangeGaindB(inputSignal types.Signal, dBChange float64) types.Signal {
 	var scale = math.Pow(10, dBChange/20)
 
@@ -37,6 +45,9 @@ func ChangeGaindB(inputSignal types.Signal, dBChange float64) types.Signal {
 }
 
 // InvertPolarity computes a polarly inverted signal of a mono signal
+//
+// Input variables:
+//  signal: Input signal to modify amplitude
 func InvertPolarity(inputSignal types.Signal) types.Signal {
 	outputSignal := ChangeGain(inputSignal, -1)
 
