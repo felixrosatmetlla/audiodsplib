c3e88a95bb932fddb4ade1e351681c858521a771
felixrosatmetlla <felixrosat@gmail.com>
2020-07-11 11:43:40 +0200

    Modify Clipping methods to use Signal type

--- distortion/clipping.go
+++ distortion/clipping.go
@@ -84,19 +84,35 @@ func HardClipping(inputSignal types.Signal, threshold float64) (types.Signal, er
 //  amplitude: The drive amount of the distortion. Range from [0, 1]:
 //   0: no distortion
 //   1: maximum amount of distortion
-func CubicDistortion(signal []float64, amplitude float64) ([]float64, error) {
+func CubicDistortion(inputSignal types.Signal, amplitude float64) (types.Signal, error) {
+	var outputSignal types.Signal
+
 	if amplitude < 0 || amplitude > 1 {
-		return []float64{}, errors.New("distortion: Invalid amplitude range. Valid range: [0, 1]")
+		outputSignal = types.Signal{
+			Data:       []float64{},
+			Channels:   inputSignal.Channels,
+			Samplerate: inputSignal.Samplerate,
+			NumSamples: 0,
+		}
+
+		return outputSignal, errors.New("distortion: Invalid amplitude range. Valid range: [0, 1]")
 	}
 
-	var bufferSize = len(signal)
-	var output = make([]float64, bufferSize)
+	var bufferSize = inputSignal.NumSamples * inputSignal.Channels
+	var outputBuffer = make([]float64, bufferSize)
 
-	for index, value := range signal {
-		output[index] = value - amplitude*(1./3.)*math.Pow(value, 3)
+	for index, value := range inputSignal.Data {
+		outputBuffer[index] = value - amplitude*(1./3.)*math.Pow(value, 3)
 	}
 
-	return output, nil
+	outputSignal = types.Signal{
+		Data:       outputBuffer,
+		Channels:   inputSignal.Channels,
+		Samplerate: inputSignal.Samplerate,
+		NumSamples: inputSignal.NumSamples,
+	}
+
+	return outputSignal, nil
 }
 
 // ArctangentDistortion distorts a signal using an arctangent function
@@ -105,17 +121,33 @@ func CubicDistortion(signal []float64, amplitude float64) ([]float64, error) {
 //  signal: Input signal to distort
 //  alpha: The drive amount of the distortion
 //   Range from [1, 10]: higher -> more distortion
-func ArctangentDistortion(signal []float64, alpha float64) ([]float64, error) {
+func ArctangentDistortion(inputSignal types.Signal, alpha float64) (types.Signal, error) {
+	var outputSignal types.Signal
+
 	if alpha < 1 || alpha > 10 {
-		return []float64{}, errors.New("distortion: Invalid alpha range. Valid range: [1, 10]")
+		outputSignal = types.Signal{
+			Data:       []float64{},
+			Channels:   inputSignal.Channels,
+			Samplerate: inputSignal.Samplerate,
+			NumSamples: 0,
+		}
+
+		return outputSignal, errors.New("distortion: Invalid alpha range. Valid range: [1, 10]")
 	}
 
-	var bufferSize = len(signal)
-	var output = make([]float64, bufferSize)
+	var bufferSize = inputSignal.NumSamples * inputSignal.Channels
+	var outputBuffer = make([]float64, bufferSize)
 
-	for index, value := range signal {
-		output[index] = (2 / math.Pi) * math.Atan(value*alpha)
+	for index, value := range inputSignal.Data {
+		outputBuffer[index] = (2 / math.Pi) * math.Atan(value*alpha)
 	}
 
-	return output, nil
+	outputSignal = types.Signal{
+		Data:       outputBuffer,
+		Channels:   inputSignal.Channels,
+		Samplerate: inputSignal.Samplerate,
+		NumSamples: inputSignal.NumSamples,
+	}
+
+	return outputSignal, nil
 }
--- distortion/clipping_test.go
+++ distortion/clipping_test.go
@@ -123,49 +123,99 @@ func TestHardClipping(t *testing.T) {
 
 func TestCubicDistortion(t *testing.T) {
 	testData := []struct {
-		inputSignal  []float64
+		inputSignal  types.Signal
 		amplitude    float64
-		outputSignal []float64
+		outputSignal types.Signal
 	}{
 		{
-			[]float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+			types.Signal{
+				Data:       []float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 			0.5,
-			[]float64{0, 0.4791666666666667, 0.8333333333333334, 0.4791666666666667, 0, -0.4791666666666667, -0.8333333333333334, -0.4791666666666667, 0},
+			types.Signal{
+				Data:       []float64{0, 0.4791666666666667, 0.8333333333333334, 0.4791666666666667, 0, -0.4791666666666667, -0.8333333333333334, -0.4791666666666667, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 		},
 		{
-			[]float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+			types.Signal{
+				Data:       []float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 			1,
-			[]float64{0, 0.4583333333333333, 0.6666666666666667, 0.4583333333333333, 0, -0.4583333333333333, -0.6666666666666667, -0.4583333333333333, 0},
+			types.Signal{
+				Data:       []float64{0, 0.4583333333333333, 0.6666666666666667, 0.4583333333333333, 0, -0.4583333333333333, -0.6666666666666667, -0.4583333333333333, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 		},
 		{
-			[]float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+			types.Signal{
+				Data:       []float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 			0,
-			[]float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+			types.Signal{
+				Data:       []float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 		},
 		{
-			[]float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+			types.Signal{
+				Data:       []float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 			-0.5,
-			[]float64{},
+			types.Signal{
+				Data:       []float64{},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 0,
+			},
 		},
 		{
-			[]float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+			types.Signal{
+				Data:       []float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 			2,
-			[]float64{},
+			types.Signal{
+				Data:       []float64{},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 0,
+			},
 		},
 	}
 
 	for _, caseData := range testData {
 		result, err := CubicDistortion(caseData.inputSignal, caseData.amplitude)
 
-		if len(result) == 0 && err == nil {
+		if result.NumSamples == 0 && err == nil {
 			t.Errorf("Error message informing of operation failure was expected and got: %v", err)
 		}
 
-		if len(result) != 0 && err != nil {
+		if result.NumSamples != 0 && err != nil {
 			t.Errorf("No error message was expected, and got %v", err)
 		}
 
-		if !audiodsputils.CompareArrayValues(result, caseData.outputSignal) {
+		if !audiodsputils.CompareSignals(result, caseData.outputSignal) {
 			t.Errorf("Cubic distortion of signal %v was incorrect, got: %v, want: %v.", caseData.inputSignal, result, caseData.outputSignal)
 		}
 	}
@@ -173,44 +223,84 @@ func TestCubicDistortion(t *testing.T) {
 
 func TestArctangentDistortion(t *testing.T) {
 	testData := []struct {
-		inputSignal  []float64
+		inputSignal  types.Signal
 		alpha        float64
-		outputSignal []float64
+		outputSignal types.Signal
 	}{
 		{
-			[]float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+			types.Signal{
+				Data:       []float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 			10,
-			[]float64{0, 0.8743340836219976, 0.9365489651388929, 0.8743340836219976, 0, -0.8743340836219976, -0.9365489651388929, -0.8743340836219976, 0},
+			types.Signal{
+				Data:       []float64{0, 0.8743340836219976, 0.9365489651388929, 0.8743340836219976, 0, -0.8743340836219976, -0.9365489651388929, -0.8743340836219976, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 		},
 		{
-			[]float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+			types.Signal{
+				Data:       []float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 			1,
-			[]float64{0, 0.2951672353008665, 0.5, 0.2951672353008665, 0, -0.2951672353008665, -0.5, -0.2951672353008665, 0},
+			types.Signal{
+				Data:       []float64{0, 0.2951672353008665, 0.5, 0.2951672353008665, 0, -0.2951672353008665, -0.5, -0.2951672353008665, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 		},
 		{
-			[]float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+			types.Signal{
+				Data:       []float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 			0,
-			[]float64{},
+			types.Signal{
+				Data:       []float64{},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 0,
+			},
 		},
 		{
-			[]float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+			types.Signal{
+				Data:       []float64{0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 9,
+			},
 			11,
-			[]float64{},
+			types.Signal{
+				Data:       []float64{},
+				Channels:   1,
+				Samplerate: 44100.0,
+				NumSamples: 0,
+			},
 		},
 	}
 
 	for _, caseData := range testData {
 		result, err := ArctangentDistortion(caseData.inputSignal, caseData.alpha)
 
-		if len(result) == 0 && err == nil {
+		if result.NumSamples == 0 && err == nil {
 			t.Errorf("Error message informing of operation failure was expected and got: %v", err)
 		}
 
-		if len(result) != 0 && err != nil {
+		if result.NumSamples != 0 && err != nil {
 			t.Errorf("No error message was expected, and got %v", err)
 		}
 
-		if !audiodsputils.CompareArrayValues(result, caseData.outputSignal) {
+		if !audiodsputils.CompareSignals(result, caseData.outputSignal) {
 			t.Errorf("Arctangent distortion of signal %v was incorrect, got: %v, want: %v.", caseData.inputSignal, result, caseData.outputSignal)
 		}
 	}
